57c42ff11b4d23b876f73012dedb20cd63918a18
Trương Ngọc Sơn <alwayssmile11a1@outlook.com>
2017-09-17 15:44:46 +0700

    Nothing much

---
+++ core/src/noshanabi/game/Screens/PlayScreen.java
@@ -0,0 +1,70 @@
+package noshanabi.game.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+import noshanabi.game.MainClass;
+
+/**
+ * Created by 2SMILE2 on 17/09/2017.
+ */
+
+public class PlayScreen implements Screen {
+
+    private MainClass game;
+    Texture texture;
+    private OrthographicCamera gameCam;
+    private Viewport gamePort;
+    public PlayScreen(MainClass game)
+    {
+        this.game = game;
+        texture = new Texture("badlogic.jpg");
+        gameCam = new OrthographicCamera();
+        gamePort = new FitViewport(800,480,gameCam);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.setProjectionMatrix(gameCam.combined);
+        game.batch.begin();
+        game.batch.draw(texture, 0,0);
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        gamePort.update(width,height);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        texture.dispose();
+    }
+}
