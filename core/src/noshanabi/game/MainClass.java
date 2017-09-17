1addef465dc006150b31e6325d46fcdbe97fe447
Trương Ngọc Sơn <alwayssmile11a1@outlook.com>
2017-09-17 10:10:58 +0700

    Nothing

--- core/src/noshanabi/game/MainClass.java
+++ core/src/noshanabi/game/MainClass.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+//Hello hello
 public class MainClass extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
